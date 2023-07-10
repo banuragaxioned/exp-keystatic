6f842d6a9569448a56a78402ad6f7185534423f5
Anurag Banerjee <anurag@axioned.com>
2023-07-10 19:20:20 +0530

    Update config to github from local

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -1,24 +1,52 @@
-import { config, collection, fields } from '@keystatic/core';
+import { config, collection, fields } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: "github",
+    repo: {
+      owner: "banuragaxioned",
+      name: "exp-keystatic",
+    },
   },
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "posts/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
+        title: fields.slug({ name: { label: "Title" } }),
         content: fields.document({
-          label: 'Content',
+          label: "Content",
           formatting: true,
           dividers: true,
           links: true,
           images: true,
         }),
+        related: fields.array(
+          fields.object({
+            name: fields.text({ label: "Name" }),
+            age: fields.integer({ label: "Age" }),
+            projects: fields.array(
+              fields.relationship({
+                label: "Posts",
+                collection: "posts",
+                validation: {
+                  isRequired: true,
+                },
+              }),
+              {
+                label: "Posts",
+                itemLabel: (props) => props.value ?? "Select a project",
+              }
+            ),
+          }),
+          // Labelling options
+          {
+            label: "Related",
+            itemLabel: (props) => props.fields.name.value,
+          }
+        ),
       },
     }),
   },
